350959c1151a2997e2685d58c2c2ecfa5bb762ca
Nea18 <nanou718@hotmail.fr>
2019-05-10 20:30:29 +0200

    Mise en place nouvelles pages front-end

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PropertyRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,8 +14,11 @@ class HomeController extends AbstractController
      *
      * @return Response
      */
-    public function index()
+    public function index(PropertyRepository $repository)
     {
-        return $this->render('pages/home.html.twig');
+        $properties = $repository->findLatest();
+        return $this->render('pages/home.html.twig', [
+            'properties' => $properties
+        ]);
     }
 }
--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,12 +12,42 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class PropertyController extends AbstractController
 {
     /**
-     * @Route("/biens", name="property.index")
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    public function __construct(PropertyRepository $repository, ObjectManager $manager)
+    {
+        $this->repository = $repository;
+    }
+  
+    /**
+      * @Route("/biens", name="property.index")
+      *
+      * @return Response
+      */
+    public function index(ObjectManager $manager)
+    {
+        $property = $this->repository->findAllVisible();
+        $manager->flush();
+        return $this->render('property/index.html.twig');
+    }
+
+    /**
+     * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug": "[a-z0-9\-]*"})
      *
      * @return Response
      */
-    public function index()
+    public function show(Property $property, string $slug)
     {
-        return $this->render('property/index.html.twig');
+        if ($property->getSlug() !== $slug) {
+            return $this->redirectToRoute('property.show', [
+                'id' => $property->getId(),
+                'slug' => $property->getSlug()
+            ], 301);
+        }
+        return $this->render('property/show.html.twig', [
+            'property' => $property
+        ]);
     }
 }
